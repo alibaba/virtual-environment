1104b45475cad0815066730d7587a5e4ab5e51a6
金戟 <jinji.lf@alibaba-inc.com>
2019-11-15 00:42:32 +0800

    add api to trigger a reconcile

--- inspect/inspect.go
+++ inspect/inspect.go
@@ -3,8 +3,11 @@ package inspect
 import (
 	"alibaba.com/virtual-env-operator/pkg/shared"
 	"github.com/labstack/echo/v4"
+	"k8s.io/apimachinery/pkg/types"
 	"net/http"
+	"os"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"strconv"
 )
 
@@ -17,6 +20,7 @@ func Start(inspectHost string, inspectPort int) {
 	// Routes
 	e.GET("/inspect/deployment", inspectDeployment)
 	e.GET("/inspect/service", inspectService)
+	e.POST("/trigger", triggerReconcile)
 
 	// Start server
 	inspectAddr := inspectHost + ":" + strconv.Itoa(inspectPort)
@@ -37,4 +41,20 @@ func inspectDeployment(c echo.Context) error {
 
 func inspectService(c echo.Context) error {
 	return c.JSON(http.StatusOK, shared.AvailableServices)
+}
+
+func triggerReconcile(c echo.Context) error {
+	name := shared.VirtualEnvIns
+	namespace := os.Getenv("WATCH_NAMESPACE")
+	if len(name) > 0 && len(namespace) > 0 {
+		_, err := (*shared.VirtualEnvController).Reconcile(reconcile.Request{
+			NamespacedName: types.NamespacedName{Name: name, Namespace: namespace},
+		})
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		} else {
+			return c.String(http.StatusOK, "virtual environment reconciled")
+		}
+	}
+	return c.String(http.StatusOK, "virtual environment not present")
 }
