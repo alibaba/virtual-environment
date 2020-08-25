109411bfbf87d4db9413765bd222c1c83ad59a79
金戟 <jinji.lf@alibaba-inc.com>
2020-08-25 18:31:27 +0800

    variable environment cannot edit dynamically, remove ticker

--- cmd/webhook/logger.go
+++ cmd/webhook/logger.go
@@ -3,30 +3,22 @@ package main
 import (
 	"log"
 	"os"
-	"time"
 )
 
 var logLevel = LEVEL_INFO
 
 func initLogger() {
-	ticker := time.NewTicker(2 * time.Minute)
-	go func(t *time.Ticker) {
-		for {
-			<-t.C
-			switch os.Getenv(CONF_LOG_LEVEL) {
-			case ERROR:
-				logLevel = LEVEL_ERROR
-				break
-			case INFO:
-				logLevel = LEVEL_INFO
-				break
-			case DEBUG:
-				logLevel = LEVEL_DEBUG
-				break
-			}
-			logDebug("Ticker:", time.Now().Format("2006-01-02 15:04:05"), ", LogLevel:", logLevel)
-		}
-	}(ticker)
+	switch os.Getenv(CONF_LOG_LEVEL) {
+	case ERROR:
+		logLevel = LEVEL_ERROR
+		break
+	case INFO:
+		logLevel = LEVEL_INFO
+		break
+	case DEBUG:
+		logLevel = LEVEL_DEBUG
+		break
+	}
 }
 
 func logError(msg ...interface{}) {
