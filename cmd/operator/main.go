f10c1591610fce3ee297810004e2cccf9dea1af3
金戟 <jinji.lf@alibaba-inc.com>
2020-11-17 15:51:07 +0800

    no need to show operation sdk version

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -46,8 +46,7 @@ var log = logf.Log.WithName("cmd")
 func printVersion() {
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
 	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
-	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
-	log.Info(fmt.Sprintf("Image version: %v", version.Version))
+	log.Info(fmt.Sprintf("Operator version: %v", version.Version))
 	log.Info(fmt.Sprintf("Image build time: %v", version.BuildTime))
 }
 
