c65793d2f7c03623cc7991101fa4788b493494e4
金戟 <jinji.lf@alibaba-inc.com>
2021-01-25 19:08:49 +0800

    add bump version script

---
+++ build/bump-to-next-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Prepare version number for next release
+
+VERSION=$(cat Makefile | grep '^VERSION \?' | sed -e 's/.* \(v[0-9.]*\)/\1/g')
+echo "Current version is: ${VERSION}"
+read -p "Next version should be: " NEXT
+
+for f in Makefile; do
+    sed -i '' "s/= ${VERSION}/= ${NEXT}/" $f
+done
+for f in deploy/global/ktenv_webhook.yaml deploy/ktenv_operator.yaml; do
+    sed -i '' "s/:${VERSION}/:${NEXT}/" $f
+done
+for f in docs/zh-cn/doc/deployment.md docs/en-us/doc/deployment.md; do
+    sed -i '' "s/${VERSION}/${NEXT}/g" $f
+done
