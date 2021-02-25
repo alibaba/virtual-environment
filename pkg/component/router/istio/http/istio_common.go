674e183ddbab7778c83f739a5b6bac4df7aceee9
金戟 <jinji.lf@alibaba-inc.com>
2021-02-25 23:51:55 +0800

    merge route should use a duplicated copy

--- pkg/component/router/istio/http/istio_common.go
+++ pkg/component/router/istio/http/istio_common.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	networkingv1alpha3 "knative.dev/pkg/apis/istio/v1alpha3"
 	"regexp"
@@ -21,9 +23,23 @@ func mergeRoute(a, b *networkingv1alpha3.HTTPRoute) (*networkingv1alpha3.HTTPRou
 	if err != nil {
 		return nil, err
 	}
-	err = json.Unmarshal(jb, &a)
+	var merged *networkingv1alpha3.HTTPRoute
+	err = deepCopy(&merged, a)
 	if err != nil {
 		return nil, err
 	}
-	return a, nil
+	err = json.Unmarshal(jb, &merged)
+	if err != nil {
+		return nil, err
+	}
+	return merged, nil
+}
+
+// deep copy an object
+func deepCopy(dst, src interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
 }
